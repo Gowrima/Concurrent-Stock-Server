218bb68e6e9875c23910043bf7851aca31c20397
apphia39 <67676029+apphia39@users.noreply.github.com>
2021-09-04 19:14:20 +0900

    Update echo.c

--- Event-based approach/echo.c
+++ Event-based approach/echo.c
@@ -1,21 +1,22 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *																 *
- * Sogang University											 *
- * Computer Science and Engineering								 *
- * System Programming											 *
- *																 *
- * Project name : Concurrent StockServer						 *
- * FIle name    : echo.c										 *
- * Author       : 20181603 kim minseon							 *
- * Date         : 2021 - 06 - 22								 *
- *																 *
+ *																 
+ * Sogang University											 
+ * Computer Science and Engineering								 
+ * System Programming											 
+ *																 
+ * Project name : Concurrent StockServer						 
+ * FIle name    : echo.c										 
+ * Author       : 20181603 kim minseon							 
+ * Date         : 2021 - 06 - 22								 
+ *																 
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "csapp.h"
 
- /* Convert number into string */
-char *num_to_str(int num)
-{
+/* *******************************************
+ * Convert number into string 
+ *********************************************/
+char *num_to_str(int num){
 	static char str[10];
 	memset(str, 0, sizeof(str));
 	sprintf(str, "%d", num);
@@ -23,11 +24,12 @@ char *num_to_str(int num)
 	return str;
 }
 
-/* Get the contents of stock_table
+/* *******************************************
+ * Get the contents of stock_table
  * - stock_table is binary tree
- * - traverse the binary tree in-order */
-void show(item* cur, char* result)
-{
+ * - traverse the binary tree in-order
+ **********************************************/
+void show(item* cur, char* result){
 	if (cur != NULL) {
 		/* 1. traverse left child */
 		show(cur->left, result);
@@ -45,18 +47,17 @@ void show(item* cur, char* result)
 	}
 }
 
-/* Search for the node */
+/* *******************************************
+ * Search for the node 
+ *********************************************/
 item* search(int id)
 {
 	item* cur = stock_table;	/* start from root node */
 
 	while (cur != NULL) {
-		if (id < cur->id)		/* less than current node, go to the left child */
-			cur = cur->left;
-		else if (id > cur->id)	/* greater than current node, go to the right child */
-			cur = cur->right;
-		else					/* equal to current node, return it */
-			break;
+		if (id < cur->id) cur = cur->left;	 /* less than current node, go to the left child */			
+		else if (id > cur->id) cur = cur->right; /* greater than current node, go to the right child */			
+		else break;				 /* equal to current node, return it */			
 	}
 
 	return cur;
@@ -64,33 +65,26 @@ item* search(int id)
 
 int buy(int id, int num)
 {
-	/* Get the node from stock_table */
-	item* cur = search(id);
-
-	/* If left_stocks are not enough */
-	if (cur->left_stock - num < 0)
+	item* cur = search(id); 		/* Get the node from stock_table */
+	
+	if (cur->left_stock - num < 0) 		/* If left_stocks are not enough */
 		return -1;
 
 	/* Update stock table */
-	//P(&mutex);				/* Block readers */
 	P(&w);					/* Block writers */
-	cur->left_stock -= num; /* Critical section */
-	//V(&mutex);				/* Unblock readers */
+	cur->left_stock -= num; 		/* Critical section */
 	V(&w);					/* Unblock writers */
 
 	return 0;
 }
 
 void sell(int id, int num)
-{
-	/* Get the node from stock_table */
-	item* cur = search(id);
+{	
+	item* cur = search(id);			/* Get the node from stock_table */
 
 	/* Update stock table */
-	//P(&mutex);				/* Block readers */
 	P(&w);					/* Block writers */
-	cur->left_stock += num; /* Critical section */
-	//V(&mutex);				/* Unblock readers */
+	cur->left_stock += num; 		/* Critical section */
 	V(&w);					/* Unblock writers */
 }
 
@@ -113,35 +107,31 @@ char *echo(char *buf)
 		stock_num = atoi(ptr);
 
 	/* Execute Command */
-	if (strcmp(cmd, "show") == 0) {		/* SHOW */
-		P(&mutex);			/* Block readers */
-		readcnt++;			/* Increase read count */
-		if (readcnt == 1)	/* If readers exist, block writers */
-			P(&w);
-		V(&mutex);			/* Unblock readers */
+	if (strcmp(cmd, "show") == 0) {	 /* SHOW */
+		P(&mutex);		 /* Block readers */
+		readcnt++;		 /* Increase read count */
+		if (readcnt == 1) P(&w); /* If readers exist, block writers */			
+		V(&mutex);		 /* Unblock readers */
 
 		show(stock_table, result);
 
-		P(&mutex);			/* Block readers */
-		readcnt--;			/* Decrease read count */
-		if (readcnt == 0)	/* If no readers, unblock writers */
-			V(&w);
-		V(&mutex);			/* Unblock writers */
+		P(&mutex);		 /* Block readers */
+		readcnt--;		 /* Decrease read count */
+		if (readcnt == 0) V(&w); /* If no readers, unblock writers */			
+		V(&mutex);		 /* Unblock writers */
 	}
 
-	else if (strcmp(cmd, "buy") == 0) {	/* BUY */
-		if (buy(id, stock_num) == -1)
-			strcpy(result, "Not enough left stocks\n");
-		else
-			strcpy(result, "[buy] success\n");
+	else if (strcmp(cmd, "buy") == 0) { /* BUY */
+		if (buy(id, stock_num) == -1) strcpy(result, "Not enough left stocks\n");
+		else strcpy(result, "[buy] success\n");
 	}
 
-	else if (strcmp(cmd, "sell") == 0) {	/* SELL */
+	else if (strcmp(cmd, "sell") == 0) { /* SELL */
 		sell(id, stock_num);
 		strcpy(result, "[sell] success\n");
 	}
 
-	else if (strcmp(cmd, "exit") == 0) {	/* EXIT */
+	else if (strcmp(cmd, "exit") == 0) { /* EXIT */
 		strcpy(result, "exit");
 	}
 
